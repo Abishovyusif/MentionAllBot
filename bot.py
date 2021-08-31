3612520ecfecf867b89d734d5d7ecca96826762f
Abishovyusif <89875188+Abishovyusif@users.noreply.github.com>
2021-09-01 01:10:53 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,20 +17,20 @@ client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
-  await event.reply("__**Im MentionAll Bot**, I can mention almost all members in group or channel 👻\nClick **/help** for more infomation__\n\n Follow [@AnjanaMadu](https://github.com/AnjanaMadu) on Github",
+  await event.reply("__**Salam mən tağ botuyam**, Məni qruplara əlavə edərək qrup üzvlərini tağ edə bilərsən 👻\nBas **/help** kömək al__\n\n Sahibimlə əlaqə : @ABISHOV_27",
                     buttons=(
-                      [Button.url('📣 Channel', 'https://t.me/harp_tech'),
-                      Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')]
+                      [Button.url('📣 Qrupumuz', 'https://t.me/darkchatgroup12'),
+                      Button.url('📦 Digər mənbələr', 'https://t.me/YusifinBiosu')]
                     ),
                     link_preview=False
                    )
 @client.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
-  helptext = "**Help Menu of MentionAllBot**\n\nCommand: /mentionall\n__You can use this command with text what you want to mention others.__\n`Example: /mentionall Good Morning!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__.\n\nFollow [@AnjanaMadu](https://github.com/AnjanaMadu) on Github"
+  helptext = "**Kömək Menyusu**\n\nTağ etmə qaydası: /mentionall\n__Tağ etmə səbəbiniz.__\n`Nümunə: /mentionall Səsliyə gəlin!`\n__Bot hərkəsi "Səsliyə gəlin" d3yərək tağ edəcək.Bütün üzvlər aiddir__.\n\nSahib [ @ABISHOV_27 ]"
   await event.reply(helptext,
                     buttons=(
-                      [Button.url('📣 Channel', 'https://t.me/harp_tech'),
-                      Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')]
+                      [Button.url('📣 Qrupumuz', 'https://t.me/darkchatgroup12'),
+                      Button.url('📦 Digər mənbələr', 'https://t.me/YusifinBiosu')]
                     ),
                     link_preview=False
                    )
@@ -38,13 +38,13 @@ async def help(event):
 @client.on(events.NewMessage(pattern="^/mentionall ?(.*)"))
 async def mentionall(event):
   if event.is_private:
-    return await event.respond("__This command can be use in groups and channels!__")
+    return await event.respond("__Bu əmri yalnızca qrup və kanallarda istifadə edə bilərsiniz!__")
   
   admins = []
   async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
     admins.append(admin.id)
   if not event.sender_id in admins:
-    return await event.respond("__Only admins can mention all!__")
+    return await event.respond("__Sadəcə adminlər bəhs edə bilərlər!__")
   
   if event.pattern_match.group(1):
     mode = "text_on_cmd"
@@ -53,11 +53,11 @@ async def mentionall(event):
     mode = "text_on_reply"
     msg = event.reply_to_msg_id
     if msg == None:
-        return await event.respond("__I can't mention members for older messages! (messages which sended before i added to group)__")
+        return await event.respond("__Əvvəlki mesajlara tağ edə bilmirəm! (Qrupa əlavə edilməmişdən əvvəlki mesajlar)__")
   elif event.pattern_match.group(1) and event.reply_to_msg_id:
     return await event.respond("__Give me one argument!__")
   else:
-    return await event.respond("__Reply to a message or give me some text to mention others!__")
+    return await event.respond("__Bir mesaja yanıt verin və ya tağ üçün səbəb yazın!__")
   
   if mode == "text_on_cmd":
     usrnum = 0
